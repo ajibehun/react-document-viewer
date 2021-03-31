55eae9a3bdf60a6177a9bcd70eb8a858061ae898
ajibehun <ajibfarhan96@gmail.com>
2021-03-31 14:28:10 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ko from "knockout";
+import "devexpress-reporting/dx-webdocumentviewer";
+import "./App.css";
+
+class ReportViewer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.reportUrl = ko.observable("TestReport");
+    this.requestOptions = {
+      host: "http://localhost:3000/",
+      // Use this line for the ASP.NET MVC backend.
+      // invokeAction: "/WebDocumentViewer/Invoke",
+      // Use this line for the ASP.NET Core backend
+      invokeAction: "DXXRDV",
+    };
+  }
+  render() {
+    return <div ref="viewer" data-bind="dxReportViewer: $data"></div>;
+  }
+  componentDidMount() {
+    ko.applyBindings(
+      {
+        reportUrl: this.reportUrl,
+        requestOptions: this.requestOptions,
+      },
+      this.refs.viewer
+    );
+  }
+  componentWillUnmount() {
+    ko.cleanNode(this.refs.viewer);
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100%", height: "1000px" }}>
+      <ReportViewer />
     </div>
   );
 }
